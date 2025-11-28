e86933fdf732e5b76512bb72f1dbebfd633feecd
Zeeshan Haider <mzeeshanh.dev@gmail.com>
2025-11-28 20:25:16 +0500

    fix: duplicate cloud image storing!

--- src/controllers/car.controller.js
+++ src/controllers/car.controller.js
@@ -9,18 +9,24 @@ import { deleteImage } from "../utils/cloudinary.js";
 
 export const createCar = async (req, res) => {
     try {
-        if (!req.file) return res.status(400).json({
-            success: false, message: "Image is required"
-        });
+        if (!req.file) {
+            return res.status(400).json({
+                success: false,
+                message: "Image is required"
+            });
+        }
+
         const carData = {
             ...req.body,
             image: req.file.path,
         };
 
         const newCar = await createCarService(carData);
+
         res.status(201).json({
             success: true,
-            message: "Car added successfully", data: newCar
+            message: "Car added successfully",
+            data: newCar,
         });
     } catch (error) {
         res.status(500).json({
@@ -36,10 +42,13 @@ export const getAllCars = async (req, res) => {
         res.status(200).json({
             success: true,
             count: cars.length,
-            data: cars
+            data: cars,
         });
     } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
     }
 };
 
@@ -47,10 +56,7 @@ export const updateCar = async (req, res) => {
     try {
         const carId = req.params.id;
         const car = await getCarByIdService(carId);
-        if (!car) return res.status(404).json({
-            success: false,
-            message: "Car not found"
-        });
+        if (!car) return res.status(404).json({ success: false, message: "Car not found" });
 
         if (req.file) {
             const oldPublicId = car.image.split("/").slice(-2).join("/").split(".")[0];
@@ -59,10 +65,11 @@ export const updateCar = async (req, res) => {
         }
 
         const updatedCar = await updateCarService(carId, req.body);
+
         res.status(200).json({
             success: true,
             message: "Car updated successfully",
-            data: updatedCar
+            data: updatedCar,
         });
     } catch (error) {
         res.status(500).json({
@@ -80,7 +87,6 @@ export const deleteCar = async (req, res) => {
             success: false,
             message: "Car not found"
         });
-
         const publicId = car.image.split("/").slice(-2).join("/").split(".")[0];
         await deleteImage(publicId);
         await deleteCarService(carId);
