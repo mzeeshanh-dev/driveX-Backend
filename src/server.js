6fbb15a62280b0adbb2b35f8a1d34adf49e88401
Zeeshan Haider <mzeeshanh.dev@gmail.com>
2025-12-01 14:38:43 +0500

    Updated Backend

--- src/server.js
+++ src/server.js
@@ -8,9 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.get('/', (res, req) => {
-    req.send("Server is running fine!!!")
+app.get('/', (req, res) => {
+    res.send("Server is running fine!!!");
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
